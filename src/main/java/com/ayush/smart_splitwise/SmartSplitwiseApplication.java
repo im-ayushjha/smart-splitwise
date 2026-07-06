74b3c5df8bd4136e31967995bc2d949beb25fbf2
im_ayushjha <ayushjha2805@gmail.com>
2026-07-06 22:40:38 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/ayush/smart_splitwise/SmartSplitwiseApplication.java
@@ -0,0 +1,14 @@
+package com.ayush.smart_splitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSplitwiseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application started");
+		SpringApplication.run(SmartSplitwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayush/smart_splitwise/SmartSplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.smart_splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
